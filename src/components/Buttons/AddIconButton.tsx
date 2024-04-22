a1fd19c926ef7eca4be1adfefddb29018e4c332c
DonggunLim <Ldonggun6766@gmail.com>
2024-04-22 21:42:17 +0900

    AddIconButton 수정

--- src/components/Buttons/AddIconButton.tsx
+++ src/components/Buttons/AddIconButton.tsx
@@ -1,9 +1,27 @@
 import { IoAddOutline } from "react-icons/io5";
 
-export default function AddIconButton() {
+type Props = {
+  size?: "small" | "medium" | "large";
+};
+
+export default function AddIconButton({ size = "medium" }: Props) {
   return (
     <button className="bg-purple-100 rounded-lg p-1 hover:bg-purple-200">
-      <IoAddOutline className="w-6 h-6" />
+      <IoAddOutline className={getSize(size)} />
     </button>
   );
 }
+
+const getSize = (size: string) => {
+  switch (size) {
+    case "small": {
+      return `w-6 h-6`;
+    }
+    case "medium": {
+      return `w-8 h-8`;
+    }
+    case "large": {
+      return `w-10 h-10`;
+    }
+  }
+};
