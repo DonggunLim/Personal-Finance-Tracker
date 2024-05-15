9579d211a8b716ee71213396de8e2d453b20d526
DonggunLim <Ldonggun6766@gmail.com>
2024-05-15 13:15:42 +0900

    Closeicon component 수정

--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -1,5 +1,33 @@
 import { CgClose } from "react-icons/cg";
 
-export default function CloseIcon() {
-  return <CgClose className="w-5 h-5" />;
+type Props = {
+  onClose?: () => void;
+  className?: string;
+  size?: "small" | "medium" | "large";
+};
+
+export default function CloseIcon({
+  onClose,
+  className,
+  size = "small",
+}: Props) {
+  return (
+    <button className={className} onClick={onClose}>
+      <CgClose className={getSize(size)} />
+    </button>
+  );
 }
+
+const getSize = (size: string) => {
+  switch (size) {
+    case "small": {
+      return `w-5 h-5`;
+    }
+    case "medium": {
+      return `w-8 h-8`;
+    }
+    case "large": {
+      return `w-11 h-11`;
+    }
+  }
+};
