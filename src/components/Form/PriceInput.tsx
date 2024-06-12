c46645dbe2c50ff2ca4f9e031973920fc2b593bd
DonggunLim <Ldonggun6766@gmail.com>
2024-06-12 22:19:39 +0900

    input cursor 맨뒤 고정

--- src/components/Form/PriceInput.tsx
+++ src/components/Form/PriceInput.tsx
@@ -1,5 +1,5 @@
 import { formatPriceToCurrency } from "@/utilities/common";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FormDataKeys } from "../ExpenseFormModal";
 
 type Props = {
@@ -9,22 +9,32 @@ type Props = {
 
 export default function PriceInput({ onChange, initialValue }: Props) {
   const [price, setPrice] = useState(initialValue || "");
-
+  const inputRef = useRef<HTMLInputElement>(null);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/[^0-9]/g, "");
     setPrice(value);
-
     onChange?.(value, "price");
+  };
+
+  const handleFocus = () => {
+    setTimeout(() => {
+      if (inputRef.current) {
+        const length = inputRef.current.value.length;
+        inputRef.current.selectionStart = inputRef.current.selectionEnd =
+          length;
+      }
+    }, 0);
   };
 
   return (
     <label>
       <div className="flex items-center px-2">
         <input
+          ref={inputRef}
           className="w-full outline-none cursor-pointer text-lg font-semibold text-right "
-          placeholder="0"
           value={formatPriceToCurrency(price)}
           onChange={handleChange}
+          onFocus={handleFocus}
         />
         <span>원</span>
       </div>
