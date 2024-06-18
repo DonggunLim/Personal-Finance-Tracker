b2c24e6b6d2e3cfecd3b76543b087d0a355610a6
DonggunLim <Ldonggun6766@gmail.com>
2024-06-18 14:26:41 +0900

    commentinput css 수정

--- src/components/Form/CommentInput.tsx
+++ src/components/Form/CommentInput.tsx
@@ -10,11 +10,16 @@ export default function CommentInput({ onChange, initialValue }: Props) {
   };
 
   return (
-    <CommonContentEditable
-      className={`box-border min-h-32 w-full rounded-md px-1 outline-none ring-1 ring-neutral-400 focus:ring-1 focus:ring-purple-400`}
-      placeholder="코멘트는 생략 가능합니다."
-      onChange={handleChange}
-      initialValue={initialValue}
-    />
+    <div className="w-full">
+      <label className="text-xs font-bold">코멘트</label>
+      <div className="text-wrap">
+        <CommonContentEditable
+          className="flex min-h-28 w-full overflow-hidden whitespace-pre-wrap break-words rounded-md px-2 py-2 text-xs font-semibold outline-none ring-1 ring-neutral-400 focus:ring-1 focus:ring-purple-400"
+          placeholder="코멘트는 생략 가능합니다."
+          onChange={handleChange}
+          initialValue={initialValue}
+        />
+      </div>
+    </div>
   );
 }
