2af3892f37e23b1229a412de790a77d4d18bf97b
DonggunLim <Ldonggun6766@gmail.com>
2024-06-20 15:52:47 +0900

    installment record 뷰 변경

--- src/components/RecordCard.tsx
+++ src/components/RecordCard.tsx
@@ -9,6 +9,7 @@ import CommonModal from "./common/CommonModal";
 import { useToast } from "@/hooks/useToast";
 import { paymentItems, tagItems } from "@/data/data";
 import { Record, RecordActionType } from "@/types/record";
+import Tooltip from "./UI/Tooltip";
 
 type Props = {
   record: Record;
@@ -20,7 +21,14 @@ export default function RecordCard({ record, manageRecord }: Props) {
   const [isPopoverShown, setIsPopoverShown] = useState(false);
   const [isEdited, setIsEdited] = useState(false);
   const [isDeleted, setIsDeleted] = useState(false);
-  const { price, description, paymentMethod, tag } = record;
+  const {
+    price,
+    description,
+    paymentMethod,
+    tag,
+    installment,
+    installmentDetails,
+  } = record;
   const payment_icon = paymentItems.filter((i) => i.title == paymentMethod)[0]
     .icon;
   const tag_icon = tagItems.filter((i) => i.title == tag)[0].icon;
@@ -95,103 +103,105 @@ export default function RecordCard({ record, manageRecord }: Props) {
 
   return (
     <div
-      className={`box group relative mb-2 flex justify-between hover:bg-slate-500 ${
-        isPopoverShown && "bg-slate-500"
-      }`}
+      className="relative my-2 rounded-2xl bg-white p-4 duration-150 ease-in hover:-translate-y-1 hover:shadow-lg"
       onMouseOver={() => setIsFocused(true)}
       onMouseLeave={() => {
         if (!isPopoverShown) setIsFocused(false);
       }}
     >
-      <div className="py-2 pl-4">
-        <p
-          className={`text-xs font-medium group-hover:text-white ${
-            isPopoverShown && "text-white"
-          }`}
-        >
-          {description}
-        </p>
-        <div className="mt-2 flex gap-2">
-          <p className="input">
-            <span>{payment_icon}</span>
-            {paymentMethod}
-          </p>
-          <p className="input">
-            <span>{tag_icon}</span>
-            {tag}
-          </p>
+      <div className="flex gap-x-2">
+        <div className="w-fit rounded-md bg-white px-2 py-1 text-xs">
+          {payment_icon}
+          {paymentMethod}
+        </div>
+        <div className="w-fit rounded-md bg-white px-2 py-1 text-xs">
+          {tag_icon}
+          {tag}
         </div>
       </div>
-      <div
-        className={`flex items-end text-xl font-bold group-hover:text-white ${
-          isPopoverShown && "text-white"
-        }`}
-      >
-        <p>{formatPriceToCurrency(price)}원</p>
-      </div>
-      {isFocused && (
-        <button
-          className="absolute right-3 top-0 cursor-pointer text-xl font-bold text-white hover:scale-125"
-          onClick={hadlePopoverClick}
-        >
-          ...
-        </button>
-      )}
-      {isPopoverShown && (
-        <Popover
-          className="absolute right-0 top-9 z-50"
-          onClose={onClosePopover}
-        >
-          <div className="flex w-36 flex-col items-center rounded-md bg-white p-1 text-xs font-extrabold">
-            <button
-              onClick={handleClickEdit}
-              className="group/edit flex w-full cursor-pointer items-center rounded-md p-1 hover:bg-purple-100"
+      <div className={`record box group mt-2 flex justify-between`}>
+        <p className={`text-xs font-medium`}>{description}</p>
+        <div className="flex items-center">
+          {installmentDetails?.isInstallment && (
+            <Tooltip
+              text={`할부 개월: ${installmentDetails.installmentPeriod}개월 
+               월 금액: ${formatPriceToCurrency(installmentDetails.installmentAmount!)}`}
             >
-              <span className="group-hover/edit:text-purple-700">
-                <LiaEdit className="h-5 w-5" />
-              </span>
-              수정
-            </button>
+              <div className="text-lg">📌</div>
+            </Tooltip>
+          )}
+          <p className="text-lg font-bold">{formatPriceToCurrency(price)}원</p>
+        </div>
+        {isFocused && (
+          <div className="absolute right-3 top-0">
             <button
-              onClick={handleClickDelete}
-              className="group/delete flex w-full cursor-pointer items-center rounded-md p-1 hover:bg-red-100"
+              className="cursor-pointer text-xl font-bold hover:scale-125"
+              onClick={hadlePopoverClick}
             >
-              <CloseIcon className="group-hover/delete:text-red-700" />
-              삭제
+              ...
             </button>
+            {isPopoverShown && (
+              <Popover
+                className="absolute -left-16 z-50 mt-2"
+                onClose={onClosePopover}
+              >
+                <div className="flex w-36 flex-col items-center rounded-md bg-white p-1 text-xs font-extrabold shadow-lg">
+                  <button
+                    onClick={handleClickEdit}
+                    className="group/edit flex w-full cursor-pointer items-center rounded-md p-1 hover:bg-purple-100"
+                  >
+                    <span className="group-hover/edit:text-purple-700">
+                      <LiaEdit className="h-5 w-5" />
+                    </span>
+                    수정
+                  </button>
+                  <button
+                    onClick={handleClickDelete}
+                    className="group/delete flex w-full cursor-pointer items-center rounded-md p-1 hover:bg-red-100"
+                  >
+                    <CloseIcon className="group-hover/delete:text-red-700" />
+                    삭제
+                  </button>
+                </div>
+              </Popover>
+            )}
           </div>
-        </Popover>
-      )}
-      {isEdited && (
-        <ExpenseFormModal
-          onClose={() => setIsEdited(false)}
-          initialFormData={record}
-          onSubmit={handleEditSubmit}
-        />
-      )}
-      {isDeleted && (
-        <CommonModal>
-          <section className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-900/30 backdrop-blur-sm">
-            <div className="relative rounded-md bg-white p-8">
-              <CloseIcon
-                className="absolute right-1 top-1 cursor-pointer"
-                onClose={() => setIsDeleted(false)}
-              />
-              <div className="flex w-72 flex-col items-center gap-y-4">
-                <p className="text-lg font-semibold text-gray-500">
-                  기록을 삭제하시겠습니까?
-                </p>
-                <button
-                  className="w-20 rounded-md bg-purple-400 px-2 py-1 text-xs font-semibold text-white"
-                  onClick={handleDeleteSubmit}
-                >
-                  삭제하기
-                </button>
+        )}
+
+        {isEdited && (
+          <ExpenseFormModal
+            onClose={() => setIsEdited(false)}
+            initialFormData={record}
+            onSubmit={handleEditSubmit}
+          />
+        )}
+        {isDeleted && (
+          <CommonModal>
+            <section className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-900/30 backdrop-blur-sm">
+              <div className="relative rounded-md bg-white p-8">
+                <CloseIcon
+                  className="absolute right-1 top-1 cursor-pointer"
+                  onClose={() => setIsDeleted(false)}
+                />
+                <div className="flex w-72 flex-col items-center gap-y-4">
+                  <p className="text-lg font-semibold text-gray-500">
+                    기록을 삭제하시겠습니까?
+                  </p>
+                  <button
+                    className="w-20 rounded-md bg-purple-400 px-2 py-1 text-xs font-semibold text-white"
+                    onClick={handleDeleteSubmit}
+                  >
+                    삭제하기
+                  </button>
+                </div>
               </div>
-            </div>
-          </section>
-        </CommonModal>
-      )}
+            </section>
+          </CommonModal>
+        )}
+        {/* <div className="alert ">
+
+        </div> */}
+      </div>
     </div>
   );
 }
